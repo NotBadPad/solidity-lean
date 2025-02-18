67e8339b71283b7263202ea73530b1663fe3b8b1
NotBadPad <awayy1432@163.com>
2025-02-18 23:02:33 +0800

    变量测试

--- .deps/test/test.sol
+++ .deps/test/test.sol
@@ -1,1 +1,10 @@
-unit256 a=1+1;
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract HelloWorld {
+
+    string sVal = "hello world";
+
+    // function syhello() public view  
+}
---
+++ .deps/test/test1.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract HelloWorld1 {
+    string sVal = "hello world";
+    uint256 intVal= 1231;
+    int intVal1 = -1231;
+    bool  boolVal = false;
+    bytes8 byteVal = "123";
+}
